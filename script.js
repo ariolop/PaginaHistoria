d7014a2ea75041dd2966ee9b0049594b9a8bab53
ariolop154 <ariolop154@g.educaand.es>
2023-11-24 10:26:48 +0100

    Inicio

---
+++ script.js
@@ -0,0 +1,15 @@
+  function cambiarTexto() {
+    document.querySelector('#contactoCambiar').innerText = 'Contacto  >';
+  }
+
+  function Texto() {
+    document.querySelector('#contactoCambiar').innerText = 'Contacto';
+  }
+
+  function cambiarTexto2() {
+    document.querySelector('#botonCambiar').innerText = "Consulte nuestras visitas diarias y reserve ahora  >";
+  }
+
+  function Texto2() {
+    document.querySelector('#botonCambiar').innerText = "Consulte nuestras visitas diarias y reserve ahora";
+  }

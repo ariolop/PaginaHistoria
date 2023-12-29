16f62b6ad4020776de9aa1bb801a58fcac52cbdd
lrodper0902 <lrodper0902@g.educaand.es>
2023-12-29 12:28:33 +0100

    añadir paginas web

---
+++ js/carousel-owl.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      autoplay:true,
+      autoplayTimeout:4000,
+      autoplaySpeed: 1500, // Cambia el intervalo de tiempo en milisegundos entre cada imagen
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:5
+          }
+      }
+    });
+  });

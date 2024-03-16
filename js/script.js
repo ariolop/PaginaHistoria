698cda5f40aeddd4c1577696bdce3e58ea9669e2
Alejandro <ariolop154@gmail.com>
2024-03-16 14:21:20 +0100

    Idiomas

--- js/script.js
+++ js/script.js
@@ -2,16 +2,17 @@
 
 document.getElementById("idioma").addEventListener("click", () => {
 
-    console.log(document.getElementById("imagenIdioma").src);
+    const imagenActual = document.getElementById("imagenIdioma").src.split("/").slice(-1);
+    console.log(imagenActual);
 
-    if(document.getElementById("imagenIdioma").src === "img/lenguajes/ingles.png")
+    if(imagenActual[0] === "ingles.png")
     {
-        document.getElementById("imagenIdioma").src = "img/lenguajes/espanol.png";
+        document.getElementById("imagenIdioma").src = "./img/lenguajes/espanol.png";
         //Función pasar página a Inglés y agregar a LocalStorage/Cookie el idioma
     }
     else
     {
-        document.getElementById("imagenIdioma").src = "img/lenguajes/ingles.png";
+        document.getElementById("imagenIdioma").src = "./img/lenguajes/ingles.png";
         //Función pasar página a Enpañol y agregar a LocalStorage/Cookie el idioma
     }
 });
